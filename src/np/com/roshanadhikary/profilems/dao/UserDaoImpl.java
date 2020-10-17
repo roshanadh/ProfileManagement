60409ef6cb22246ac42b47943284535f88449e0b
Roshan <nahsorad@gmail.com>
2020-10-17 13:55:17 +0545

    Implement getUsers() of UserDao

---
+++ src/np/com/roshanadhikary/profilems/dao/UserDaoImpl.java
@@ -0,0 +1,71 @@
+package np.com.roshanadhikary.profilems.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import np.com.roshanadhikary.profilems.dto.User;
+import np.com.roshanadhikary.profilems.util.DbUtil;
+
+public class UserDaoImpl implements UserDao {
+	
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getUsers() {
+		List<User> users = new ArrayList<>();
+		try {
+			Connection con = DbUtil.getConnection();
+			String query = "SELECT username, name, email, address FROM users";
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				User user = new User();
+				user.setUsername(rs.getString("username"));
+				user.setUsername(rs.getString("name"));
+				user.setUsername(rs.getString("email"));
+				user.setUsername(rs.getString("address"));
+				users.add(user);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return users;
+	}
+
+	@Override
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeUser(String username) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
