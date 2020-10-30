99455fda50c5fbd9e780d98d83ca484c831d7e75
Roshan <nahsorad@gmail.com>
2020-10-30 19:13:13 +0545

    Pass loginError query to index.jsp

--- src/np/com/roshanadhikary/profilems/controllers/LoginController.java
+++ src/np/com/roshanadhikary/profilems/controllers/LoginController.java
@@ -12,15 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import np.com.roshanadhikary.profilems.dao.UserDaoImpl;
+import np.com.roshanadhikary.profilems.dto.User;
 import np.com.roshanadhikary.profilems.services.UserService;
 import np.com.roshanadhikary.profilems.services.UserServiceImpl;
 import np.com.roshanadhikary.profilems.util.PasswordHash;
 
 /**
- * Servlet implementation class SignInController
+ * Servlet implementation class LoginController
  */
-@WebServlet("/SignInController")
-public class SignInController extends HttpServlet {
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private UserService userService;
@@ -28,7 +29,7 @@ public class SignInController extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SignInController() {
+    public LoginController() {
         super();
         userService = new UserServiceImpl(new UserDaoImpl());
     }
@@ -37,48 +38,79 @@ public class SignInController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// use for signing out a user
-		HttpSession session = request.getSession(false);
-		session.invalidate();
+		String servletPath = request.getServletPath();
+		HttpSession session = null;
+		RequestDispatcher rd;
 		
-		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-		rd.forward(request, response);
+		if (servletPath.equals("/logout")) {
+			// use for signing out a user
+			session = request.getSession(false);
+			session.invalidate();
+			
+			rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+		} else if (servletPath.equals("/login")) {
+			// check if the user is already logged in, ...
+			// ... if they're: redirect to their profile page ...
+			// ... if they're not: redirect to the login page
+			session = request.getSession(false);
+			
+			if (
+				session != null &&
+				session.getAttribute("loggedInStatus") != null &&
+				session.getAttribute("username") != null &&
+				(boolean) session.getAttribute("loggedInStatus")
+				)
+			{
+				// redirect to the profile page
+				rd = request.getRequestDispatcher("profile.jsp");
+				
+				User user = userService.getUserByUsername((String) session.getAttribute("username"));
+				
+				request.setAttribute("user", user);
+				rd.forward(request, response);
+			} else {
+				// redirect to the login page
+				rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request, response);
+			}
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// use for signing in a user
+		RequestDispatcher rd = null;
 		try {
-			System.out.println("ok iam here");
 			String username = request.getParameter("username");
 			String password = request.getParameter("password");
-			
-//			byte[] hashedBytes = PasswordHash.getHash(password);
-//			String hashedPassword = new String(hashedBytes, StandardCharsets.UTF_8);
-			
-//			System.out.println("Hashed Password: " + hashedPassword);
+
 			if (userService.getUserByUsername(username) == null) {
-				System.out.println("User by the username '" + username + "' does not exist!");
+				System.out.println("Username doesn't exist!");
+				rd = request.getRequestDispatcher("?loginError=unknown-username");
+				rd.forward(request, response);
 				return;
 			}
 			
 			if (!userService.isRegistered(username, password)) {
-				System.out.println("The credentials do not match");
+				System.out.println("Bad credentials!");
+				rd = request.getRequestDispatcher("?loginError=bad-credentials");
+				rd.forward(request, response);
 				return;
 			}
 			
 			HttpSession session = request.getSession();
 			session.setAttribute("loggedInStatus", true);
 			session.setAttribute("username", username);
-			System.out.println("session[username] set:  " + session.getAttribute("username"));
 			
+			System.out.println("session[username] set:  " + session.getAttribute("username"));
 			System.out.println("Welcome, " + username + "!");
 			
-			RequestDispatcher rd = request.getRequestDispatcher("profile.jsp");
+			rd = request.getRequestDispatcher("profile.jsp");
 			request.setAttribute("user", userService.getUserByUsername(username));
-			rd.include(request, response);
+			rd.forward(request, response);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
